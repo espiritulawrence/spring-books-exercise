327fc350c96934f0f3ab111a9cf5f4a400bd8ab8
espiritulawrence <lawrenceespiritu@gmail.com>
2021-10-22 03:08:29 +0800

    Added Global and Local exception handling.

--- src/main/java/com/example/spring_books_exercise/controller/BookController.java
+++ src/main/java/com/example/spring_books_exercise/controller/BookController.java
@@ -1,18 +1,15 @@
 package com.example.spring_books_exercise.controller;
 
+import com.example.spring_books_exercise.exception.global.BookNotFoundException;
+import com.example.spring_books_exercise.exception.global.UnableToAddException;
+import com.example.spring_books_exercise.exception.global.UnableToDeleteException;
 import com.example.spring_books_exercise.model.Book;
 import com.example.spring_books_exercise.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -24,24 +21,35 @@ public class BookController {
 
     @GetMapping("/all")
     public List<Book> getAllBooks() {
+
         return bookService.retrieveAllBooks();
     }
 
     @GetMapping("/{id}")
     public Book getBookById(@PathVariable int id) {
-        return bookService.retrieveBooksById(id);
+        if (bookService.retrieveBooksById(id) == null) {
+            throw new BookNotFoundException();
+        } else {
+
+        }return bookService.retrieveBooksById(id);
     }
 
     @PostMapping(value = "/", produces = "application/json")
     public ResponseEntity<Book> addBook(@RequestBody Book newBook) {
-        bookService.addNewBook(newBook);
-        return new ResponseEntity<>(HttpStatus.OK);
+        if (newBook.getTitle() == null || newBook.getTitle().equals("") ||
+                newBook.getAuthor() == null || newBook.getAuthor().equals("")) {
+            throw new UnableToAddException();
+        }
+        else {
+            bookService.addNewBook(newBook);
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/")
     public  ResponseEntity<Book> updateBookById(@RequestBody Book updatedBook) {
         if (bookService.retrieveBooksById(updatedBook.getId()) == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            throw new BookNotFoundException();
         } else {
             bookService.updateBook(updatedBook);
         }
@@ -51,7 +59,7 @@ public class BookController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Book> deleteBookById(@PathVariable int id) {
         if (bookService.retrieveBooksById(id) == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            throw new UnableToDeleteException();
         } else {
             bookService.deleteBookById(id);
         }
